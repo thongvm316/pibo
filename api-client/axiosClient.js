4d1d60e9796ad9b465ae5e8eeb266eb2333f65d6
thongvm316 <vominhthong1603@gmail.com>
2022-03-07 14:51:57 +0700

    config axiosClient for call api

---
+++ api-client/axiosClient.js
@@ -0,0 +1,40 @@
+import axios from "axios"
+import queryString from "query-string"
+
+const axiosClient = axios.create({
+  headers: {
+    "content-type": "application/json",
+  },
+  paramsSerializer: (params) => queryString.stringify(params),
+})
+
+axiosClient.interceptors.request.use(
+  (config) => {
+    const pauthToken = localStorage.getItem("pauthToken")
+    if (pauthToken) {
+      config.headers.Authorization = `Bearer ${pauthToken}`
+    }
+
+    return config
+  },
+  (error) => Promise.reject(error)
+)
+
+axiosClient.interceptors.response.use(
+  (response) => {
+    if (response?.headers?.pauth) {
+      localStorage.setItem("pauthToken", response.headers.pauth)
+    }
+
+    if (response && response.data) {
+      return response.data
+    }
+
+    return response
+  },
+  (error) => {
+    return Promise.reject(error)
+  }
+)
+
+export default axiosClient
