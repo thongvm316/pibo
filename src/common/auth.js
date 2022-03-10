cef883340100a13179f156ec5bab9c041c0d3e6f
Gary <tien.pm@cj.net>
2022-03-10 11:57:32 +0700

    conflict

--- src/common/auth.js
+++ src/common/auth.js
@@ -1,16 +1,25 @@
 import { useAuth } from '@/hooks/use-auth';
 import React, { useEffect } from 'react';
 import { useRouter } from 'next/router';
+import { CircularProgress } from '@mui/material';
+import { Box } from '@mui/system';
 
 export default function Auth({ children }) {
   const router = useRouter();
-  const { user, firstTimeLoading } = useAuth();
+  const { user, firstTimeLoading, error } = useAuth();
 
   useEffect(() => {
-    if (!firstTimeLoading && !user?.id) router.push('/login');
+    if (!firstTimeLoading && !user?.userList) {
+      router.push('/login');
+    }
   }, [router, user, firstTimeLoading]);
 
-  if (!user?.id) return <p>Loading....</p>;
+  if (!user?.userList)
+    return (
+      <Box>
+        <CircularProgress />
+      </Box>
+    );
 
   return <div>{children}</div>;
 }
