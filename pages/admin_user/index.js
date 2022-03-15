fb35d966b420343d1ed5f002d571c11a2219951b
Khoa <khoanguyen.k15@gmail.com>
2022-03-15 14:44:08 +0700

    Config AdminUser page

--- pages/admin_user/index.js
+++ pages/admin_user/index.js
@@ -3,32 +3,10 @@ import { useAuth } from '@/hooks/use-auth';
 import BaseCard from '@/src/components/baseCard/BaseCard';
 import FullLayout from '@/src/layouts/FullLayout';
 import { useEffect } from 'react';
+import gridConfigs from './gridConfigs';
 
 const AdminUser = () => {
   const { user } = useAuth();
-
-  const columnLayout = [
-    {
-      dataField: 'userId',
-      headerText: '관리자 계정',
-    },
-    {
-      dataField: 'userNm',
-      headerText: '관리자 권한',
-    },
-    {
-      dataField: 'acctUsgYn',
-      headerText: '잠금 여부',
-    },
-    {
-      dataField: 'lastLoginDate',
-      headerText: '마지막 로그인일시',
-    },
-    {
-      dataField: 'lastModifyDate',
-      headerText: '최종 수정일시',
-    },
-  ];
 
   useEffect(async () => {
     const test = await axiosClient.get('/pibo/api/users', {
@@ -40,16 +18,11 @@ const AdminUser = () => {
     });
 
     if (test) {
-      const gridProps = {
-        showRowNumColumn: false,
-        autoGridHeight: true,
-        usePaging: true,
-        pagingMode: 'simple',
-        // pagingBottomGap: 10,
-        pageRowCount: 10,
-      };
-
-      const createDataGrid = AUIGrid.create('#table_data', columnLayout, gridProps);
+      const createDataGrid = AUIGrid.create(
+        '#table_data',
+        gridConfigs.columnLayout,
+        gridConfigs.gridProps
+      );
       AUIGrid.setGridData(createDataGrid, test.userList);
       AUIGrid.bind(createDataGrid, 'pageChange', async (event) => {
         console.log('event', event);
