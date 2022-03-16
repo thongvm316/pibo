527e3cdb4fef42f34c647ae40bb904db40c628c3
Gary <tien.pm@cj.net>
2022-03-16 12:46:00 +0700

    add new filter

--- pages/pims_prd_1.js
+++ pages/pims_prd_1.js
@@ -1,6 +1,5 @@
 import FullLayout from '@/components/Layout/FullLayout';
 import TestRender from '@/components/TestRender';
-import CustomDatePicker from '@/components/DateRangePicker';
 import { Box } from '@mui/material';
 
 const Prd1 = () => {
@@ -10,7 +9,6 @@ const Prd1 = () => {
         urlImg="https://images.pexels.com/photos/1280638/pexels-photo-1280638.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
         content="uptatum, fames incididunt, mus eius, diam senectus veniam justo omnis fugit, occaecat pharetra maiores, minus auctor? Bibendum autem dolores ridiculus dui magna. Amet dolor mauris porro, eveniet"
       />
-      <CustomDatePicker />
     </Box>
   );
 };
