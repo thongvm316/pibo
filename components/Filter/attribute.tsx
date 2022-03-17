3db6435444f30073add118cbeafe59678d02b873
Gary <tien.pm@cj.net>
2022-03-17 14:52:21 +0700

    no message

--- components/Filter/attribute.tsx
+++ components/Filter/attribute.tsx
@@ -2,15 +2,16 @@ import React from 'react';
 import { Autocomplete, TextField } from '@mui/material';
 import FilterSection from '.';
 import axiosClient from '@/api-client/axiosClient';
+import { GetStaticProps, GetServerSideProps } from 'next';
 
 const options = [
   { label: 'The Godfather', id: 1 },
   { label: 'Pulp Fiction', id: 2 },
 ];
 
-const Attribute = async () => {
-  const attrOptions = await axiosClient.get('/pibo/pims/pip/attribute-groups');
-  console.log(attrOptions);
+const Attribute = ({ data }: any) => {
+  console.log('abc', data);
+
   return (
     <FilterSection title="속성그룹" label="All">
       <Autocomplete
@@ -25,3 +26,13 @@ const Attribute = async () => {
 };
 
 export default Attribute;
+
+export const getStaticProps: GetStaticProps = async () => {
+  const res = await axiosClient.get('/pibo/pims/bo/pip/attribute-groups');
+  const data = res.data;
+  return {
+    props: {
+      data: data,
+    },
+  };
+};
