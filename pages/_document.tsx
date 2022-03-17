50d38e83a4088afc3d7ee9c42109eeba03a3bd64
Gary <tien.pm@cj.net>
2022-03-17 14:05:13 +0700

    no message

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,9 +17,9 @@ export default class MyDocument extends Document {
           <meta httpEquiv="Content-Style-Type" content="text/css" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
             rel="stylesheet"
-          ></link>
+          />
           <link rel="shortcut icon" href="/static/images/logos/logo.png" />
           {/* Inject MUI styles first to match with the prepend: true configuration. */}
           {(this.props as any).emotionStyleTags}
