7f11a04965e8a5cf6b8a5c055289c07df4e84119
Gary <tien.pm@cj.net>
2022-03-17 10:22:21 +0700

    no message

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,11 +18,11 @@ const Home: NextPageWithLayout = () => {
     <Box>
       <Grid container spacing={0}>
         <Grid item xs={12} lg={12}>
-          <SalesOverview />
+          {/* <SalesOverview /> */}
         </Grid>
         {/* ------------------------- row 1 ------------------------- */}
         <Grid item xs={12} lg={4}>
-          <DailyActivity />
+          {/* <DailyActivity /> */}
         </Grid>
         <Grid item xs={12} lg={8}>
           {/* <ProductPerfomance /> */}
