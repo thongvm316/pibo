9e5f82aa5ca6ccff7086e7ce00ad3c335dda3711
Khoa <khoanguyen.k15@gmail.com>
2022-03-17 09:45:47 +0700

    Config AdminUser page

--- context/auth.js
+++ context/auth.js
@@ -2,6 +2,8 @@ import React, { createContext, useState, useContext, useEffect } from 'react';
 import Cookies from 'js-cookie';
 import { useRouter } from 'next/router';
 import axiosClient from '@/api-client/axiosClient';
+import authApi from '@/api-client/authApi';
+import userApi from '@/api-client/userApi';
 
 //api here is an axios instance which has the baseURL set according to the env.
 
@@ -19,17 +21,20 @@ export const AuthProvider = ({ children }) => {
       console.log('hello');
       const pauth = Cookies.get('pauth');
       const pid = Cookies.get('pid');
+
       if (pauth) {
         try {
           console.log("Got a token in the cookies, let's see if it is valid");
           axiosClient.defaults.headers.Authorization = `Bearer ${pauth}`;
-          const { data: user } = await axiosClient.get(`/pibo/api/user/${pid}`);
+          const { data: user } = await authApi.getUser(pid);
+
           if (user) {
             setUser(user);
           } else {
             router.replace('/login');
           }
-          const { data: menu } = await axiosClient.get(`/pibo/api/menu`);
+
+          const { data: menu } = await userApi.menuApi();
           localStorage.setItem('menuList', JSON.stringify(menu));
           if (menu) setMenu(menu.menuList);
         } catch (err) {
@@ -38,25 +43,33 @@ export const AuthProvider = ({ children }) => {
       } else {
         router.replace('/login');
       }
+
       setLoading(false);
     }
+
     loadUserFromCookies();
   }, []);
 
   const login = async (data) => {
-    const { data: res, headers } = await axiosClient.post('/pibo/api/login', data);
+    const { data: res, headers } = await authApi.loginApi(data);
+
     if (res.result === 'S') {
       Cookies.set('pauth', headers?.pauth, { expires: 60 });
       Cookies.set('pid', headers?.pid, { expires: 60 });
       axiosClient.defaults.headers.Authorization = `Bearer ${headers?.pauth}`;
       // axiosClient.defaults.headers.pid = `Bearer ${headers?.pid}`;
-      const { data: user } = await axiosClient.get(`/pibo/api/user/${headers?.pid}`);
-      const { data: menu } = await axiosClient.get(`/pibo/api/menu`);
+      const { data: user } = await authApi.getUser(headers?.pid);
+      const { data: menu } = await userApi.menuApi();
+
       setUser(user);
       setMenu(menu.menuList);
       localStorage.setItem('menuList', JSON.stringify(menu));
+
       return true;
+    } else {
+      alert(res.message);
     }
+
     setFirstTimeLogin(false);
     return false;
   };
