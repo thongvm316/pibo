b1e205d5ccd48901072d02509e913abc74f5e153
Khoa <khoanguyen.k15@gmail.com>
2022-03-17 14:24:28 +0700

    Update AdminUser page

--- pages/admin_user/index.js
+++ pages/admin_user/index.js
@@ -4,7 +4,7 @@ import FullLayout from '@/components/Layout/FullLayout';
 import authApi from '@/api-client/authApi';
 import { useAuth } from '@/context/auth';
 import { Box, Modal, Typography } from '@mui/material';
-import gridConfigs from './gridConfigs';
+import { columnLayout, gridProps } from './gridConfigs';
 
 const AdminUser = () => {
   const { isAuthenticated } = useAuth();
@@ -13,38 +13,36 @@ const AdminUser = () => {
   const [open, setOpen] = useState(false);
   const [detailUser, setDetailUser] = useState({});
 
-  useMemo(() => {
-    const columnLayout = [
-      {
-        dataField: 'userId',
-        headerText: '관리자 계정',
-        renderer: {
-          type: 'LinkRenderer',
-          linkField: 'userId',
-          baseUrl: 'javascript',
-          jsCallback: () => {
-            setOpen(true);
-          },
-        },
-      },
-      {
-        dataField: 'userNm',
-        headerText: '관리자 권한',
-      },
-      {
-        dataField: 'acctUsgYn',
-        headerText: '잠금 여부',
-      },
-      {
-        dataField: 'lastLoginDate',
-        headerText: '마지막 로그인일시',
-      },
-      {
-        dataField: 'lastModifyDate',
-        headerText: '최종 수정일시',
-      },
-    ];
-  },[]);
+  // const columnLayout = [
+  //   {
+  //     dataField: 'userId',
+  //     headerText: '관리자 계정',
+  //     renderer: {
+  //       type: 'LinkRenderer',
+  //       linkField: 'userId',
+  //       baseUrl: 'javascript',
+  //       jsCallback: () => {
+  //         setOpen(true);
+  //       },
+  //     },
+  //   },
+  //   {
+  //     dataField: 'userNm',
+  //     headerText: '관리자 권한',
+  //   },
+  //   {
+  //     dataField: 'acctUsgYn',
+  //     headerText: '잠금 여부',
+  //   },
+  //   {
+  //     dataField: 'lastLoginDate',
+  //     headerText: '마지막 로그인일시',
+  //   },
+  //   {
+  //     dataField: 'lastModifyDate',
+  //     headerText: '최종 수정일시',
+  //   },
+  // ];
 
   useEffect(() => {
     if (isAuthenticated) {
@@ -52,7 +50,7 @@ const AdminUser = () => {
         try {
           const { data } = await authApi.getUserList();
           const getId = testData.current.id;
-          const tableData = AUIGrid.create(`#${getId}`, columnLayout, gridConfigs.gridProps);
+          const tableData = AUIGrid.create(`#${getId}`, columnLayout, gridProps);
           AUIGrid.setGridData(tableData, data.userList);
           AUIGrid.bind(tableData, 'pageChange', async () => {
             await authApi.getUserList();
